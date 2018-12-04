7414040d5f0ebcb8b3041d60169bd55620269617
RickCas <rmacaskill@me.com>
2018-12-04 08:38:08 +0000

    tweaks

--- SQL Data Catalog/GetListOfDatabases.ps1
+++ SQL Data Catalog/GetListOfDatabases.ps1
@@ -1,22 +1,29 @@
 
 
 $ServerRootUrl = 'http://rm-win10-sql201.testnet.red-gate.com:15156'
+$AuthToken = 'NTE3NjA0OTQ0NjE0Nzg1MDI0Ojc5NzViY2YwLTAyOGUtNGU4My1hZjY4LTJkNWE0ZmI4MmNlMw=='
+
 $AddUrl = "$ServerRootUrl/api/instances"
 
 $Databases=@("");
 
+$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$Headers.Add("Authorization", "Bearer $AuthToken")
+
 $Response = Invoke-RestMethod -Uri $AddUrl `
-    -UseDefaultCredentials `
-    -Method Get 
+    -Headers $Headers `
+    -Method Get `
+    -AllowUnencryptedAuthentication
    
 $Response.ForEach{
     $InstanceId =  $_.instance[0].id ;
     Write-Host $_.instance[0].name ;
     $AddUrl = "$ServerRootUrl/api/instances/$InstanceId/databases";
     $DbResponse = Invoke-RestMethod -Uri $AddUrl `
-    -UseDefaultCredentials `
-    -Method Get 
-    $DbResponse;
+    -Headers $Headers `
+    -Method Get `
+    -AllowUnencryptedAuthentication 
+    
     $Databases.Add( $DbResponse.database[0].name);
             };
 
