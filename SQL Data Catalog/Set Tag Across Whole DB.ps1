78e32314be4c62bad9bf65b7e3db4fddff304663
Richard Macaskill <richard.macaskill@outlook.com>
2018-12-19 14:33:31 +0000

    a

--- SQL Data Catalog/Set Tag Across Whole DB.ps1
+++ SQL Data Catalog/Set Tag Across Whole DB.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <#
     Script to tag all empty tables as general/not used. $authToken should be replaced with
     a token generated from the appropriate data catalog intallation.
@@ -110,40 +109,3 @@ foreach ($t in $tableList)
             -Method Put -Body $postJson -ContentType 'application/json' 
     }
 }
-=======
-
-
-$ServerRootUrl = 'http://rm-win10-sql201.testnet.red-gate.com:15156'
-$AuthToken = 'NTE3NjA0OTQ0NjE0Nzg1MDI0Ojc5NzViY2YwLTAyOGUtNGU4My1hZjY4LTJkNWE0ZmI4MmNlMw=='
-
-$AddUrl = "$ServerRootUrl/api/instances"
-
-$Databases=@("");
-
-$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$Headers.Add("Authorization", "Bearer $AuthToken")
-
-# Get list of instances
-$Response = Invoke-RestMethod -Uri $AddUrl `
-    -Headers $Headers `
-    -Method Get `
-    -AllowUnencryptedAuthentication
-   
-# For each instance, get list of databases
-$Response.ForEach{
-    $InstanceId =  $_.instance[0].id ;
-    Write-Host $_.instance[0].name ;
-    $AddUrl = "$ServerRootUrl/api/instances/$InstanceId/databases";
-    $DbResponse = Invoke-RestMethod -Uri $AddUrl `
-    -Headers $Headers `
-    -Method Get `
-    -AllowUnencryptedAuthentication 
-    
-    $Databases.Add( $DbResponse.database[0].name);
-            };
-
-$Databases.count;
-
-
-Write-Host "Return Status Code: $($Response.StatusCode)"
->>>>>>> d21791a4ef8c69abd89e742b199a252395dfff9e
