f3f68f96f6bcdb7c5cd239f6c9b105a61a0154fa
Richard Macaskill <richard.macaskill@outlook.com>
2019-09-24 13:39:22 +0100

    Update ExamineColumns.ps1

--- SQL Data Catalog/ExamineColumns.ps1
+++ SQL Data Catalog/ExamineColumns.ps1
@@ -1,36 +1,26 @@
-Invoke-WebRequest -Uri 'http://rm-win10-sql201.testnet.red-gate.com:15156/powershell' -OutFile 'data-catalog.psm1' -Headers @{"Authorization"="Bearer NTM2OTUxMTYyNzA4OTUxMDQwOmRiNjIyYWMxLWI1NDYtNDQzNi04OTE2LWQ1MzkxNGIzYzI5MQ=="}
+#config
+$dataCatalogServer = "http://rm-win10-sql201.testnet.red-gate.com:15156"
+$dataCatalogAuthToken = "NTM2OTUxMTYyNzA4OTUxMDQwOmRiNjIyYWMxLWI1NDYtNDQzNi04OTE2LWQ1MzkxNGIzYzI5MQ=="
+
+#retrieve modules from data catalog api
+Invoke-WebRequest -Uri "$dataCatalogServer/powershell" -OutFile "datacatalog.psm1" -Headers @{"Authorization" = "Bearer $dataCatalogAuthToken" }
  
-Import-Module .\data-catalog.psm1 -Force
+#import data catalog modules
+Import-Module .\datacatalog.psm1 -Force
 
 $instanceName = 'rm-iclone1.testnet.red-gate.com'
 $databaseName = 'StackOverflow2010'
-$authToken = "NTM2OTUxMTYyNzA4OTUxMDQwOmRiNjIyYWMxLWI1NDYtNDQzNi04OTE2LWQ1MzkxNGIzYzI5MQ=="
 
 # connect to your SQL Data Catalog instance - you'll need to generate an auth token in the UI
-Use-Classification -ClassificationAuthToken $authToken 
+Use-Classification -ClassificationAuthToken $dataCatalogAuthToken -ServerUrl $dataCatalogServer
 
 # get all columns into a collection
 $allColumns = Get-Columns -instanceName $instanceName -databaseName $databaseName 
 
 #$allColumns | Format-Table
 
-"Columns returned: "+  $allColumns.Count #17443
+"{0} columns returned." -f $allColumns.Count 
 
-$emptyTableColumns = $allColumns | Where-Object {$_.tableRowCount -eq 0 } # 722
+$emptyTableColumns = $allColumns | Where-Object { $_.tableRowCount -eq 0 } 
 
-$specificMaskableColumns = $maskableColumns | Where-Object {$_.tags.name  -notcontains  "Masked TBD"} #173
-
-$specificMaskableColumns.schemaName | Select-Object -Unique    
-<# 
-OLAP
-Release
-Reporting
-Staging
-Transform 
-#>
-$specificMaskableColumns.tags.name | Select-string  "Masked" | Select-Object -Unique
-<# 
-Masked EmailAddress
-Masked ForeName
-Masked DateOfBirth #>
-
+"There are {0} empty tables" -f $emptyTableColumns.Count
